1a159205162b99f094d0792ae39dfcd239c94ac6
Aman Mourya <amanmourya09072003@gmail.com>
2026-03-30 02:36:46 +0530

    added register debug logs

--- controllers/register.js
+++ controllers/register.js
@@ -1,40 +1,50 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+
+  console.log("DATA RECEIVED 👉", email, name, password); // 👈 ADDED (input check)
+
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
+
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
+  console.log("HASH GENERATED 👉", hash); // 👈 ADDED (bcrypt check)
+
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
       .into('login')
       .returning('email')
       .then(loginEmail => {
+        console.log("LOGIN INSERT SUCCESS 👉", loginEmail); // 👈 ADDED
+
         return trx('users')
           .returning('*')
           .insert({
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // loginEmail[0] --> this used to return the email
-            // TO
-            // loginEmail[0].email --> this now returns the email
             email: loginEmail[0].email,
             name: name,
             joined: new Date()
           })
           .then(user => {
+            console.log("USER INSERT SUCCESS 👉", user); // 👈 ADDED
             res.json(user[0]);
-          })
+          });
       })
       .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
+      .catch(err => {
+        console.log("REGISTER ERROR 👉", err); // 👈 MOST IMPORTANT (real error yahi milega)
+        trx.rollback();
+        res.status(400).json('unable to register');
+      });
+  })
+    .catch(err => {
+      console.log("TRANSACTION ERROR 👉", err); // 👈 ADDED (outer error)
+      res.status(400).json('unable to register');
+    });
 }
 
 module.exports = {
   handleRegister: handleRegister
 };
-
-
