1d027e309fe48583f48b5dc7dc5deb70696ceb19
Aman Mourya <amanmourya09072003@gmail.com>
2026-03-30 03:04:21 +0530

    clean register controller

--- controllers/register.js
+++ controllers/register.js
@@ -1,14 +1,11 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
 
-  console.log("DATA RECEIVED 👉", email, name, password); // 👈 ADDED (input check)
-
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
 
   const hash = bcrypt.hashSync(password);
-  console.log("HASH GENERATED 👉", hash); // 👈 ADDED (bcrypt check)
 
   db.transaction(trx => {
     trx.insert({
@@ -18,8 +15,6 @@ const handleRegister = (req, res, db, bcrypt) => {
       .into('login')
       .returning('email')
       .then(loginEmail => {
-        console.log("LOGIN INSERT SUCCESS 👉", loginEmail); // 👈 ADDED
-
         return trx('users')
           .returning('*')
           .insert({
@@ -28,21 +23,16 @@ const handleRegister = (req, res, db, bcrypt) => {
             joined: new Date()
           })
           .then(user => {
-            console.log("USER INSERT SUCCESS 👉", user); // 👈 ADDED
             res.json(user[0]);
           });
       })
       .then(trx.commit)
-      .catch(err => {
-        console.log("REGISTER ERROR 👉", err); // 👈 MOST IMPORTANT (real error yahi milega)
+      .catch(() => {
         trx.rollback();
         res.status(400).json('unable to register');
       });
   })
-    .catch(err => {
-      console.log("TRANSACTION ERROR 👉", err); // 👈 ADDED (outer error)
-      res.status(400).json('unable to register');
-    });
+    .catch(() => res.status(400).json('unable to register'));
 }
 
 module.exports = {
